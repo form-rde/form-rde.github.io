fd09faa5b2e2296cdf89d439cc930cbd9046de87
form-rde <138497730+form-rde@users.noreply.github.com>
2023-07-17 11:21:05 -0300

    Update envio.php

--- envio.php
+++ envio.php
@@ -4,13 +4,9 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
     // Configurar o e-mail
     $destinatario = 'lmmaciel@toyota.com.br';
     $assunto = 'Mensagem do formulário de contato';
-    $headers = "From: server";
 
     // Enviar o e-mail
-    mail($destinatario, $assunto, $headers);
-
-    // Redirecionar o usuário de volta para o formulário
-    header('Location: formulario.html');
+    mail($destinatario, $assunto);
     exit();
 }
 ?>
